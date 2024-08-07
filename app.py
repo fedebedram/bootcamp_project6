41f0c8240d3389c93fcc257a4a1c9554ab22c030
Federico Bedolla Ramos <fede.bedram@gmail.com>
2024-08-07 17:43:39 -0600

    Adición de gráficas y de .streamlit para conectar a un servidor web de manera adecuada.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Importación de librerías
 import pandas as pd
-import plotly.express as ps
+import plotly.express as px
 import streamlit as st
 
 # Importación del df
@@ -15,3 +15,50 @@ car_data["cylinders"] = car_data["cylinders"].astype("Int64")
 car_data["odometer"] = car_data["odometer"].astype("Int64")
 car_data["date_posted"] = pd.to_datetime(
     car_data["date_posted"], format="%Y-%m-%d")
+
+# Título de la página web
+st.markdown("""
+    <style>
+    .header{
+        text-align: center;
+        font-size: 60px;
+        font-family: 'Copperplate';
+        color: #C1C9C2;
+        font-weight: italic;
+        padding-bottom: 50px;
+        }
+    </style>
+    <div class="header">Análisis vehicular"</div>
+""")
+
+# Botón de gráfica para la creación del histograma
+hist_button = st.button("Construir un histograma")
+
+if hist_button:
+    st.write("Creación de histograma con la dispersión del kilometraje de los vehículos")
+    fig = px.histogram(
+        car_data,
+        x="odometer",
+        color="season",
+        bargap=.005,
+        xref="odometer",
+        yref="cylinders",
+        fillcolor="PaleteTurquoise",
+        line_color="LightGray"
+        )
+    st.plotly_chart(fig, use_container_width=True)
+
+# Botón de gráfica para la creación del histograma
+scat_button = st.button("Construir un gráfico de dispersión")
+
+if scat_button:
+    st.write("")
+    fig = px.scatter(
+        car_data,
+        x="model_year",
+        y="price",
+        xaxis_title="Año del modelo",
+        yaxis_title="Precio",
+        color="season"
+        )
+    st.plotly_chart(fig, use_container_width=True)
