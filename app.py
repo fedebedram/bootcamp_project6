d915ad2569e9e1ecfc0896618b8c92373fa008b5
Federico Bedolla Ramos <fede.bedram@gmail.com>
2024-08-09 10:47:40 -0600

    Adición de títulos para las secciones de la página

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ car_data["date_posted"] = pd.to_datetime(
     car_data["date_posted"], format="%Y-%m-%d")
 car_data["price"] = car_data["price"].astype("float")
 car_data["manufacturer"] = car_data["model"].str.split().str[0]
+car_data["manufacturer"] = car_data["manufacturer"].str.capitalize()
 car_data["model"] = car_data["model"].str.split().str[1]
 
 # Título de la página web
@@ -33,6 +34,19 @@ st.markdown("""
     <div class="header">Análisis vehicular</div>
 """, unsafe_allow_html=True)
 
+st.markdown("""
+    <style>
+    .h1{
+        text-align: center;
+        font-size: 35px;
+        font-family: 'Copperplate';
+        color: #C1C9C2;
+        padding-bottom: 30px;
+        }
+    </style>
+    <div class="h1">Uso de casillas de verificación</div>
+""", unsafe_allow_html=True)
+
 car_data_top = car_data[car_data["price"]>=90000.0]
 car_data_top = car_data_top.reset_index(drop=True)
 car_data_top = car_data_top.reindex(["price", "manufacturer",
@@ -45,17 +59,39 @@ car_data_top = car_data_top.reindex(["price", "manufacturer",
                                      ],
                                      axis=1)
 
+car_data_0 = car_data[car_data["odometer"]==0]
+car_data_0 = car_data_0.reset_index(drop=True)
+car_data_0 = car_data_0.reindex(["price", "manufacturer",
+                                "model", "model_year",
+                                "condition", "cylinders",
+                                "odometer", "transmission",
+                                "type", "fuel", "paint_color",
+                                "is_4wd", "date_posted", 
+                                "days_listed"
+                                ],
+                                axis=1)
+
 # Casilla de verificación con información relevante
-table_button = st.checkbox("Mostrar tabla")
+table_button = st.checkbox("Mostrar tabla de los vehículos más caros")
+table_button2 = st.checkbox("Mostrar tabla de los vehículos menos usados")
 
 if table_button:
     st.write(
         "Los 20 vehículos más caros de la tienda")
     st.dataframe(car_data_top, use_container_width=True)
+if table_button2:
+    st.write(
+        "Vehículos con 0 kilometraje")
+    st.dataframe(car_data_top, use_container_width=True)
 
+st.markdown("""
+    <div class="h1">Uso de botones</div>
+""", unsafe_allow_html=True)
 
 # Botón de gráfica para la creación de gráfico de caja
-box_button = st.button("Construir un gráfico de caja")
+col1, col2 = st.columns(2)
+with col1:
+    box_button = st.button("Construir un gráfico de caja")
 
 if box_button:
     st.write(
@@ -64,13 +100,18 @@ if box_button:
     st.plotly_chart(fig, use_container_width=True)
 
 # Botón de gráfica para la creación de gráfico de violin
-vio_button = st.button("Construir un gráfico de violin")
+with col2:
+    vio_button = st.button("Construir un gráfico de violin")
 
 if vio_button:
     st.write("Relación estre condición y año del vehículo")
     fig = px.violin(car_data, x='model_year', y='condition', color='condition',
                  title='Condición vs Año del Modelo')
     st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("""
+    <div class="h1">Uso casillas de selección</div>
+""", unsafe_allow_html=True)
 
 # Comparación de precios entre coches de una marca
 manufacturera = st.selectbox("Seleccione un fabricante:", car_data["manufacturer"].unique())
@@ -90,8 +131,8 @@ if len(filtro) >= 2:
 
 
 # Comparación de días de rotación entre marcas
-man1 = st.selectbox("Seleccione una compañía:", car_data["manufacturer"].unique())
-man2 = st.selectbox("Seleccione otra compañía:", car_data["manufacturer"].unique())
+man1 = st.selectbox("Seleccione una compañía vehicular:", car_data["manufacturer"].unique())
+man2 = st.selectbox("Seleccione otra compañía vehicular:", car_data["manufacturer"].unique())
 
 if (car_data["manufacturer"].isin([man1]).any()) and (car_data["manufacturer"].isin([man2]).any()):
 
